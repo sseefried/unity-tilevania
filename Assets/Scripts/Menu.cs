76387c68c3a978cee6360417b37dde2d7d184062
Sean Seefried <sean.seefried@gmail.com>
2020-03-14 12:31:55 +1000

    Add success screen

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,9 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    const int mainMenuIndex = 0;
+    const int level1Index = 1;
+
     // Start is called before the first frame update
     public void StartFirstLevel()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(level1Index);
     }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuIndex);
+    }
+
 }
